6f95f32bf88b557343e85b0db4be4e3d9a3c3df4
Bruno Antunes <bmvantunes@gmail.com>
2020-04-08 21:20:26 +0100

    a bit of design

---
+++ src/pages/_app.js
@@ -0,0 +1,56 @@
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import App from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+import theme from './theme';
+
+export default class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>My page</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <AppBar position="fixed">
+          <Toolbar variant="dense">
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">Microphone Shop</Typography>
+          </Toolbar>
+        </AppBar>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Container>
+            <Box marginTop={8}>
+              <Component {...pageProps} />
+            </Box>
+          </Container>
+        </ThemeProvider>
+      </React.Fragment>
+    );
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,61 @@
-import { GetStaticProps } from "next";
+import { Grid } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { GetStaticProps } from 'next';
 import Link from 'next/link';
-import { Microphone } from "../../model/Microphone";
-import { openDB } from "../openDB";
+import { Microphone } from '../../model/Microphone';
+import { openDB } from '../openDB';
 
 export interface IndexProps {
   microphones: Microphone[];
 }
 
-export default function Index({microphones}: IndexProps) {
-  return <div>
-    {microphones.map(microphone => (
-      <Link href="/[id]" as={`/${microphone.id}`}>
-        <a>{microphone.brand + microphone.model}</a>
-      </Link>
-    ))}
-  </div>
+export default function Index({ microphones }: IndexProps) {
+  return (
+    <Grid container spacing={3} >
+      {microphones.map((microphone) => (
+        <Grid item xs={12} sm={3}>
+          <Link href="/microphone/[id]" as={`/microphone/${microphone.id}`}>
+            <a>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={microphone.brand + ' ' + microphone.model}
+                    height="300"
+                    image={microphone.imageUrl}
+                    title={microphone.brand + ' ' + microphone.model}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {microphone.brand + ' ' + microphone.model}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </a>
+          </Link>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
-export const getStaticProps: GetStaticProps = async ctx => {
+export const getStaticProps: GetStaticProps = async (ctx) => {
   const db = await openDB();
   const microphones = await db.all('select * from microphone');
 
-  return { props: {microphones}};
-}
+  return { props: { microphones } };
+};
